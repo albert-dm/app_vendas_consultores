bfc29ee1790242cc284249d380760f0c367ee80d
albert-dm <albert-dm@outlook.com>
2018-06-09 10:22:43 -0300

    Consultores service separado

--- app/scripts/controllers/indicacoes.consultor.controller.js
+++ app/scripts/controllers/indicacoes.consultor.controller.js
@@ -3,12 +3,14 @@
 angular.module('ambaya')
 .controller('IndicacoesConsultorController',[ '$scope', 'consultoresService', function($scope, consultoresService){
     $scope.carregaDados();
-    consultoresService.indicados($scope.usuario._id).then(                
+    $scope.indicados = [];
+    $scope.indicados = ['teste'];
+   /*  consultoresService.indicados($scope.usuario._id).then(                
         function(res){
             $scope.indicados = res.data;
         },
         function(res){
              Materialize.toast("Falha ao carregar indicados!", 5000, 'notificacaoRuim');
         }
-    );
+    ); */
 }]);
